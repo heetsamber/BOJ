16bed17ac895b1fe90153938ca25db6491700a38
heetsamber <103236108+heetsamber@users.noreply.github.com>
2022-12-07 20:57:48 +0900

    Update boj10799.kt

--- Kt/Stack/boj10799.kt
+++ Kt/Stack/boj10799.kt
@@ -1,1 +1,20 @@
+import java.util.Stack
 
+fun main(args: Array<String>){
+    val str = readLine()!!
+    var cnt : Int = 0
+    val S = Stack<Char>()
+
+    for(i in 0 until str.length){
+        if(str[i] == '('){
+            S.push(str[i]);
+        }else if(str[i] == ')' && str[i-1] == '('){    // 레이저 일떄
+            S.pop()
+            cnt += S.size
+        }else{
+            cnt++
+            S.pop()
+        }
+    }
+    println(cnt)
+}
