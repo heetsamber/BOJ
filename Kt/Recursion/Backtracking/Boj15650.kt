024a0ffb4b708e957af28341c795be17be460588
heetsamber <103236108+heetsamber@users.noreply.github.com>
2022-12-16 08:20:23 +0900

    Update Boj15650.kt

--- Kt/Recursion/Backtracking/Boj15650.kt
+++ Kt/Recursion/Backtracking/Boj15650.kt
@@ -1,1 +1,29 @@
+fun main() {
+    val (n, m) = readln().split(" ").map { it.toInt() }
+    val arr: IntArray = IntArray(10)
+    val isUsed: Array<Boolean> = Array<Boolean>(10) { i -> false }
+    fun sol(c: Int) {
+        if (c == m) {
+            for (i in 0 until m) {
+                print("${arr[i]} ")
+            }
+            println()
+            return
+        }
+        var start = 1
+        if (c != 0) {
+            start = arr[c - 1] + 1
+        }
+        for (i in start..n) {
+            if (!isUsed[i]) {
+                arr[c] = i
+                isUsed[i] = true
+                sol(c + 1)
+                isUsed[i] = false
+            }
+        }
+    }
+    sol(0)
+}
+
 
