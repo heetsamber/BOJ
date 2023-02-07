3c2b5c25b3a7d2a6714df318f20e55f49eba3f5a
heetsamber <103236108+heetsamber@users.noreply.github.com>
2023-02-07 23:14:54 +0900

    Update priority_queue_boj11286.java

--- Java/DataStructure/priority_queue_boj11286.java
+++ Java/DataStructure/priority_queue_boj11286.java
@@ -1,1 +1,9 @@
+import java.util.PriorityQueue;
+import java.util.Collections;
 
+
+public class Main{
+    public static void main(String[] args){
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+    }
+}
